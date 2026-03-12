602ba7929bb0dc8f555450601ee971bab5d47fc8
DUnny <brandodd@mail.uc.edu>
2026-03-12 02:48:15 -0400

    Add birth month program

---
+++ README.md
@@ -0,0 +1,25 @@
+# BirthMonth
+
+This program asks the user for their birth month as an integer from 1 to 12.
+
+If the number is in range, it prints:
+`Your birth month is: N`
+
+If the number is not in range, it prints:
+`You entered an incorrect month value: N`
+
+## Test Runs
+
+Valid input:
+
+```text
+5
+Your birth month is: 5
+```
+
+Invalid input:
+
+```text
+13
+You entered an incorrect month value: 13
+```
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int birthMonth = input.nextInt();
+
+        if (birthMonth >= 1 && birthMonth <= 12) {
+            System.out.println("Your birth month is: " + birthMonth);
+        } else {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
+
+        input.close();
+    }
+}
