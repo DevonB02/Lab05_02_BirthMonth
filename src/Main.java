1f6285df1346282c8eb8c7f97182bd2405d9e153
DUnny <brandodd@mail.uc.edu>
2026-03-12 03:00:25 -0400

    Birth Month

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter your birth month: ");
         int birthMonth = input.nextInt();
 
         if (birthMonth >= 1 && birthMonth <= 12) {
             System.out.println("Your birth month is: " + birthMonth);
         } else {
-            System.out.println("You entered an incorrect month value: " + birthMonth);
+            System.out.println("You entered an incorrect value: " + birthMonth);
         }
 
         input.close();
